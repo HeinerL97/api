902e178e869cf122f3c2560efbc185a537ce7a63
HeinerL97 <h.leonardo321@gmail.com>
2025-10-30 21:27:22 -0500

    fix: Correct typo in gunicorn package name

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    print("🚀 Dynamic Flask API with MongoDB running. Access your resources at http://127.0.0.1:5000/<resource_name>")
     print("🚀 Dynamic Flask API with in-memory DB running. Access your resources at http://127.0.0.1:5000/<resource_name>")
     app.run(debug=True, host='0.0.0.0', port=5000)
